949e4d2c73e5b67130e50273c5ed00531451bc20
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-07-28 15:27:36 +0200

    configured DB connection.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,33 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PantryModule } from './pantry/pantry.module';
 import { AdminModule } from './admin/admin.module';
 import { AuthModule } from './auth/auth.module';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [UserModule, PantryModule, AdminModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UserModule,
+    PantryModule,
+    AdminModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
---
+++ src/config/config.example.ts
@@ -0,0 +1,11 @@
+// export const config = {
+//   corsOrigin: undefined,
+//   appPort: 3001,
+//   db: {
+//     host: 'localhost',
+//     port: 3306,
+//     username: 'username',
+//     password: 'password',
+//     database: 'database',
+//   }
+// };
