9d083ce6171052a001fe6b40326c740e57365a11
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-07-28 15:16:30 +0200

    created basic file structure.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { PantryModule } from './pantry/pantry.module';
+import { AdminModule } from './admin/admin.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, PantryModule, AdminModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
