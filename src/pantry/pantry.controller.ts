3eaefb8e11934771a24dbf0035ceb7955b301b41
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-08-18 11:13:01 +0200

    created DTO file and added new routes.

--- src/pantry/pantry.controller.ts
+++ src/pantry/pantry.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { PantryService } from './pantry.service';
 
 @Controller('pantry')
-export class PantryController {}
+export class PantryController {
+  constructor(private pantryService: PantryService) {}
+
+  @Get('/')
+  getAllPantries() {
+    return this.pantryService.getAllPantries();
+  }
+
+  @Post('/')
+  addPantry() {
+    return this.pantryService.addPantry();
+  }
+}
--- src/pantry/pantry.service.ts
+++ src/pantry/pantry.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PantryService {}
+export class PantryService {
+  async getAllPantries(): Promise<any> {
+    return 'getAllPantries';
+  }
+
+  async addPantry(): Promise<any> {
+    return 'addPantry';
+  }
+}
