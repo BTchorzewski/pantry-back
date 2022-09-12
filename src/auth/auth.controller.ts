edd0d833b2f6b52c2ab3437b40c4b2f8f035375b
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-09-12 14:40:44 +0200

    crated and implemented decorator for User object in request.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,30 @@
-import { Controller, forwardRef, Inject } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  forwardRef,
+  Inject,
+  Post,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { LocalAuthGuard } from '../guards/local-auth.guard';
+import { UserObj } from '../decorators/userObj.decorators';
 @Controller('auth')
 export class AuthController {
   constructor(
-    @Inject(forwardRef(() => AuthService)) authService: AuthService,
+    @Inject(forwardRef(() => AuthService)) private authService: AuthService,
   ) {}
+
+  @Post('/login')
+  @UseGuards(LocalAuthGuard)
+  login(@UserObj() user): Promise<any> {
+    return this.authService.login(user);
+  }
+
+  @Post('/logout')
+  logout(): Promise<any> {
+    return this.authService.logout();
+  }
 }
