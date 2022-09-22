2a97bd9f1858f08695e9a64fb057b1b4a1669813
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-09-22 11:13:00 +0200

    created mock user id

---
+++ src/decorators/mockUserId.decorator.ts
@@ -0,0 +1,5 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const MockUserId = createParamDecorator(() => {
+  return '0f0834ce-83b1-41a2-b1e1-69f8d80a1c60';
+});
