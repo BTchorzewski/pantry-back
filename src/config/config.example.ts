de3af36ed26d4c164894d6c296233770eca9d1c7
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-10-21 21:16:01 +0200

    updated the configuraton file.

--- src/config/config.example.ts
+++ src/config/config.example.ts
@@ -1,15 +1,25 @@
-// export const config = {
-//   corsOrigin: undefined,
-//   appPort: 3001,
-//   db: {
-//     host: 'localhost',
-//     port: 3306,
-//     username: 'username',
-//     password: 'password',
-//     database: 'database',
-//   },
-//   jwt: {
-//     accessToken: 'access token secrete key',
-//     refreshToken: 'refresh token secrete key',
-//   },
-// };
+export const config = {
+  corsOrigin: undefined,
+  appPort: 3001,
+  db: {
+    host: 'localhost',
+    port: 3306,
+    username: 'username',
+    password: 'password',
+    database: 'database',
+  },
+  jwt: {
+    accessToken: 'access token secrete key',
+    refreshToken: 'refresh token secrete key',
+  },
+  // Important! email properties are set up for the smtp server from the documentation.
+  // change it if you want to use your own smtp server.
+  email: {
+    host: 'localhost',
+    port: 2525,
+    auth: {
+      user: '',
+      password: '',
+    },
+  },
+};
