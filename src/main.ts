d932f93edb6cdaef4bf3846d712c3f4062abf8f6
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-12-06 14:36:02 +0100

    removed schemas

--- src/main.ts
+++ src/main.ts
@@ -42,7 +42,9 @@ async function bootstrap() {
     )
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: { defaultModelsExpandDepth: -1, url: '/' },
+  });
 
   await app.listen(3001);
 }
